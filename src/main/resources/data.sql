c3fa4a655e96b533bf75398cdf97a71807a32fcc
abcpaem <abcpaem@gmail.com>
2022-07-25 13:44:03 +0100

    Removes user role for admin

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -326,5 +326,4 @@ INSERT INTO role (`id`,`name`,`description`) VALUES (2,'USER', 'API user');
 
 
 INSERT INTO user_role (`user_id`,`role_id`) VALUES (1,1);
-INSERT INTO user_role (`user_id`,`role_id`) VALUES (1,2);
 INSERT INTO user_role (`user_id`,`role_id`) VALUES (2,2);
